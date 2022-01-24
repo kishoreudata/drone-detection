9ff8891ffef2fe60efb0977adf09e77ac75c4278
kishoreudata <36151863+kishoreudata@users.noreply.github.com>
2022-01-24 12:42:50 +0530

    Add files via upload

--- drone_detect.py
+++ drone_detect.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import cv2
 from core import Core
@@ -7,17 +6,18 @@ c = Core()
 
 
 cap = cv2.VideoCapture(0)
-
+c.set_model(c.get_model())
 while (True):
     ret, frame =cap.read()
     #image = c.load_image_by_path(frame)
     #drawing_image = c.get_drawing_image(image)
-    image1 = frame.copy()
+    #image1 = frame.copy()
+    image1 = frame
     image = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
 
     processed_image, scale = c.pre_process_image(image)
 
-    c.set_model(c.get_model())
+
     boxes, scores, labels = c.predict_with_graph_loaded_model(processed_image, scale)
 
     #detections = c.draw_boxes_in_image(drawing_image, boxes, scores)
@@ -29,4 +29,3 @@ while (True):
         break
 cap.release()
 cv2.destroyAllWindows()
-
